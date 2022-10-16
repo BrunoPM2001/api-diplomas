fbf5ab8500212ca4acc57ab3084d1c6fb6742385
Bruno P.M <brunoporras2001@gmail.com>
2022-10-16 18:45:55 -0500

    Configuración del multer

---
+++ src/configs/multer.js
@@ -0,0 +1,35 @@
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: "/temp/xlsx",
+  filename: function (req, file, cb) {
+    cb(null, 'padron.xlsx')
+  }
+})
+
+const uploadConfig = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype == "application/vnd.ms-excel" || file.mimetype == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
+      cb(null, true) 
+    } else {
+      cb(new Error ('El archivo no es del tipo adecuado'))
+    }
+  }
+}).single('File')
+
+const upload = (req, res, next) => {
+  try {
+    uploadConfig(res, (err) => {
+      if (err) {
+        res.json({ message: "Fail", detail: "Error en la carga del padrón." })
+      } else {
+        next()
+      }
+    })
+  } catch (e) {
+    res.json({ message: "Fail", detail: "Exception" })
+  }
+}
+
+export default upload
