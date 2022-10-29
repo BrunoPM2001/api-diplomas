031a90a1b4e977bc02719b1eb9933c39a917eff3
Bruno P.M <brunoporras2001@gmail.com>
2022-10-29 10:12:40 -0500

    Index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import padronesRoutes from './src/routes/padrones.js'
+import usuariosRoutes from './src/routes/usuarios.js'
 
 const app = express()
 
@@ -12,5 +13,6 @@ app.use(cors())
 
 //  Rutas
 app.use("/api/padrones", padronesRoutes)
+app.use("/api/usuarios", usuariosRoutes)
 
 app.listen(process.env.PORT || 3000, () => console.log("Listen to port 3000"))
