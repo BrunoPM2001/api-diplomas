5613ce35ae7ec37f249ca984f0f94eaf395141f0
Bruno P.M <brunoporras2001@gmail.com>
2022-11-02 20:35:35 -0500

    Creación de padrones en base a excels - ok

--- src/controllers/padrones.controller.js
+++ src/controllers/padrones.controller.js
@@ -1,10 +1,80 @@
+import { PrismaClient } from '@prisma/client'
 import { promisify } from 'util'
 import fs from 'fs'
+import jwt from 'jsonwebtoken'
 import { deleteEmptyRows, getDataFromExcel, generatePdf } from '../utils/generatePdf.js'
+import { cleanExcelPadron } from '../utils/cleanerPadron.js'
 
+const prisma = new PrismaClient()
 const unlinkAsync = promisify(fs.unlink)
 const ctrl = {}
 
+ctrl.getAll = (req, res) => {
+  try {
+    jwt.verify(req.header("Authorization"), process.env.JWT_KEY, async (error, user) => {
+      if (error) {
+        res.json({ message: "Fail", detail: "Token inválido" })
+      } else {
+        const padrones = await prisma.padrones.findMany()
+        res.json({ message: "Success", padrones })
+      }
+    })
+  } catch (e) {
+    res.json({ message: "Fail", detail: "Exception" })
+  }
+}
+
+ctrl.getOne = (req, res) => {
+  try {
+    jwt.verify(req.header("Authorization"), process.env.JWT_KEY, async (error, user) => {
+      if (error) {
+        res.json({ message: "Fail", detail: "Token inválido" })
+      } else {
+        const {
+          registro
+        } = req.query
+        const padron = await prisma.padrones.findUnique({
+          where: {
+            REG_REGISTRO: registro
+          }
+        })
+        res.json({ message: "Success", padron })
+      }
+    })
+  } catch (e) {
+    res.json({ message: "Fail", detail: "Exception" })
+  }
+}
+
+ctrl.generate = (req, res) => {
+  try {
+    jwt.verify(req.header("Authorization"), process.env.JWT_KEY, async (error, user) => {
+      if (error) {
+        res.json({ message: "Fail", detail: "Token inválido" })
+      } else {
+        const file = req.file
+        if (file == undefined) {
+          res.json({ message: "Fail", detail: "No se ha seleccionado ningún archivo" })
+        } else {
+          //  Obtener el archivo subido y la data
+          await deleteEmptyRows(req.file.path)
+          const data = await getDataFromExcel(req.file.path)
+          //  Eliminar archivo del servidor
+          await unlinkAsync(req.file.path)
+          //  Eliminar atributos innecesarios del padrón
+          const newData = cleanExcelPadron(data, user.dni)
+          const padrones = await prisma.padrones.createMany({
+            data: newData,
+            skipDuplicates: true
+          })
+          res.json({ message: "Success", detail: "Se han registrado " + padrones.count + " nuevos padrones"  })
+        }
+      }
+    })
+  } catch (e) {
+    res.json({ message: "Fail", detail: "Exception" })
+  }
+}
 ctrl.previsualizarDiplomaSegunPadron = async (req, res) => {
   try {
     const file = req.file
