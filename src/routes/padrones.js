20222230ea86302698c98e648b9a18ca88b2b3b6
Bruno P.M <brunoporras2001@gmail.com>
2022-11-02 20:36:05 -0500

    Rutas para los padrones

--- src/routes/padrones.js
+++ src/routes/padrones.js
@@ -2,8 +2,23 @@ import { Router } from 'express'
 import upload from '../configs/multer.js'
 import controller from '../controllers/padrones.controller.js'
 
-const route = Router()
+const router = Router()
 
-route.post("/previzualizarDiplomaSegunPadron", upload, controller.previsualizarDiplomaSegunPadron)
+router.get("/getAll", controller.getAll)
 
-export default route
+router.get("/getOne", controller.getOne)
+
+router.post("/generateFromExcel", upload, controller.generate)
+
+//  no usar por el momento
+router.post("/createPadron", )
+
+router.put("/updatePadron", )
+
+router.delete("/deletePadron", )
+
+router.post("/loadPadron", upload)
+
+router.post("/previsualizarDiplomaSegunPadron", upload, controller.previsualizarDiplomaSegunPadron)
+
+export default router
